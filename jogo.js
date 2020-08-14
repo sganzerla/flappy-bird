ca7dd0411f843b502f879f62ae17ba8431464ee6
Alisson Michel Sganzerla <alisson_sganzerla@yahoo.com.br>
2020-08-14 00:24:11 -0300

    :bookmark_tabs: background sprites

--- jogo.js
+++ jogo.js
@@ -5,6 +5,37 @@ const canvas = document.querySelector('canvas');
 
 const contexto = canvas.getContext('2d');
 
+const planoDeFundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
+
+    desenha() {
+
+        contexto.fillStyle = '#70c5ce';
+        contexto.fillRect(0, 0, canvas.width, canvas.height);
+
+        contexto.drawImage(
+            sprites, // image,
+            planoDeFundo.spriteX, planoDeFundo.spriteY, // sprite x, sprite y,
+            planoDeFundo.largura, planoDeFundo.altura, // tamanho da sprite sWidth, sHeight,
+            planoDeFundo.x, planoDeFundo.y, // posicao dentro do canvas
+            planoDeFundo.largura, planoDeFundo.altura // tamanho dentro do canvas dWidth, dHeight 
+        );
+        contexto.drawImage(
+            sprites, // image,
+            planoDeFundo.spriteX, planoDeFundo.spriteY, // sprite x, sprite y,
+            planoDeFundo.largura, planoDeFundo.altura, // tamanho da sprite sWidth, sHeight,
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y, // posicao dentro do canvas
+            planoDeFundo.largura, planoDeFundo.altura // tamanho dentro do canvas dWidth, dHeight 
+        );
+    }
+
+}
+
 const chao = {
 
     spriteX: 0,
@@ -26,14 +57,11 @@ const chao = {
             sprites, // image,
             chao.spriteX, chao.spriteY, // sprite x, sprite y,
             chao.largura, chao.altura, // tamanho da sprite sWidth, sHeight,
-            chao.x + chao.largura, chao.y, // posicao dentro do canvas
+            (chao.x + chao.largura), chao.y, // posicao dentro do canvas
             chao.largura, chao.altura // tamanho dentro do canvas dWidth, dHeight 
         );
     }
-
 }
-
-
 
 const flappyBird = {
     spriteX: 0,
@@ -59,7 +87,9 @@ const flappyBird = {
 function loop() {
 
     flappyBird.desenha();
+    planoDeFundo.desenha();
     chao.desenha();
+
     requestAnimationFrame(loop);
 }
 
