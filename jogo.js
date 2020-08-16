ada1ffae99152aff73555d7ad3493f5ad12c128d
Alisson Michel Sganzerla <alisson_sganzerla@yahoo.com.br>
2020-08-16 00:58:55 -0300

    :checkered_flag: move pipe

--- jogo.js
+++ jogo.js
@@ -200,24 +200,94 @@ function mudaParaTela(novaTela) {
     }
 }
 
+function criaCanos() {
+
+    const canos = {
+
+        largura: 52,
+        altura: 400,
+        chao: {
+            spriteX: 0,
+            spriteY: 169,
+        },
+        ceu: {
+            spriteX: 52,
+            spriteY: 169,
+        },
+        espaco: 80,
+        desenha() {
+
+            canos.pares.forEach(function (par) {
+                const yRandom = par.y;
+                const espacamentoEntreCanos = 90;
+                // cano ceu
+                const canoCeuX = par.x;
+                const canoCeuY = yRandom;
+                contexto.drawImage(
+                    sprites,
+                    canos.ceu.spriteX, canos.ceu.spriteY,
+                    canos.largura, canos.altura,
+                    canoCeuX, canoCeuY,
+                    canos.largura, canos.altura
+                );
+
+                const canoChaoX = par.x;
+                const canoChaoY = canos.altura + espacamentoEntreCanos + yRandom;
+
+                contexto.drawImage(
+                    sprites,
+                    canos.chao.spriteX, canos.chao.spriteY,
+                    canos.largura, canos.altura,
+                    canoChaoX, canoChaoY,
+                    canos.largura, canos.altura
+                );
+            });
+        },
+        pares: [],
+        atualiza() {
+            const passou100Frames = frames % 100 === 0;
+            if (passou100Frames) {
+                canos.pares.push({
+                    x: canvas.width,
+                    y: -150 * (Math.random() + 1),
+                });
+            }
+
+            canos.pares.forEach(function (par) {
+                par.x = par.x - 2;
+
+                if (par.x + canos.largura <= 0) {
+                    canos.pares.shift();
+                }
+            });
+        }
+    };
+
+    return canos;
+}
+
+
 const Telas = {
 
     INICIO: {
         inicializa() {
             globais.flappyBird = criaFlappyBird();
             globais.chao = criaChao();
+            globais.canos = criaCanos();
         },
         desenha() {
             planoDeFundo.desenha();
             globais.chao.desenha();
             globais.flappyBird.desenha();
-            mensagemGetReady.desenha();
+            globais.canos.desenha();
+            //  mensagemGetReady.desenha();
         },
         click() {
             mudaParaTela(Telas.JOGO);
         },
         atualiza() {
             globais.chao.atualiza();
+            globais.canos.atualiza();
         }
     },
 
@@ -237,7 +307,6 @@ const Telas = {
     }
 
 }
-
 
 
 
